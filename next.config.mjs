7b1dcc71d888c5202222ab6491150d9cf2b8d970
Bsjaehee <bsjaehee94@gmail.com>
2024-09-26 19:24:55 +0900

    add: ChampionPage 데이터 불러오기

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ddragon.leagueoflegends.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
     formats: ['image/avif', 'image/webp'],
   },
--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import Image from 'next/image';
+import { type Champion } from '../../types/Champion';
 
-const ChampionsPage = () => {
-  return <div>ChampionsPage</div>;
+const ChampionsPage = async () => {
+  //SSG
+  const res = await fetch(
+    'https://ddragon.leagueoflegends.com/cdn/14.19.1/data/ko_KR/champion.json',
+    {
+      cache: 'force-cache',
+    }
+  );
+  const data = await res.json();
+  const champions: Champion[] = Object.values(data.data);
+  console.log('ch--->', champions);
+  return (
+    <div className='grid grid-cols-4 gap-5'>
+      {champions.map((c: Champion) => (
+        <ul key={c.id} className='bg-gray-600 text-white'>
+          <Image
+            src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/champion/${c.image.full}`}
+            width={200}
+            height={200}
+            alt={c.name}
+          />
+          <li>{c.name}</li>
+          <li>{c.title}</li>
+        </ul>
+      ))}
+    </div>
+  );
 };
 
 export default ChampionsPage;
