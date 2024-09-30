b45c1b40782f6cf999c7ac476becc1aa14929cbf
Bsjaehee <bsjaehee94@gmail.com>
2024-09-30 20:34:22 +0900

    fix: 로테이션 페이지 loading중 수정

---
+++ src/app/items/loading.tsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <div className='text-center mt-10'>
+      <h2 className='text-xl font-bold mb-4'>
+        아이템 목록을 불러오는 중입니다...
+      </h2>
+      <p>잠시만 기다려 주세요.</p>
+    </div>
+  );
+};
+
+export default Loading;
--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -9,72 +9,79 @@ import Link from 'next/link';
 
 const RotationPage = () => {
   const [rotationChampions, setRotationChampions] = useState<Champion[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const rotationResponse = await fetch('/api/rotation');
-      if (!rotationResponse.ok) {
-        console.log('rotationResponse 에러');
-        return;
+      try {
+        const rotationResponse = await fetch('/api/rotation');
+        if (!rotationResponse.ok) {
+          console.log('rotationResponse 에러');
+          return;
+        }
+
+        const rotationData: ChampionRotation = await rotationResponse.json();
+        const rotationChampionIds: RotationIds = rotationData.freeChampionIds;
+
+        // 전체 챔피언 정보 불러오기
+        const allChampions = await getChampionData();
+        const championsArray: Champion[] = Object.values(allChampions);
+
+        const championsDetails: (Champion | undefined)[] = await Promise.all(
+          rotationChampionIds.map(async (id) => {
+            const championDetail = championsArray.find(
+              (all) => all.key === id.toString()
+            );
+            console.log(championDetail);
+
+            if (!championDetail) {
+              console.log(`ID ${id}에 해당하는 챔피언을 찾을 수 없습니다.`);
+            }
+            return championDetail;
+          })
+        );
+        // undefined배열일 때 제외하는 필터링
+        const filteredChampions = championsDetails.filter(
+          // 타입 가드
+          (champion): champion is Champion => champion !== undefined
+        );
+
+        setRotationChampions(filteredChampions);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
       }
-
-      const rotationData: ChampionRotation = await rotationResponse.json();
-      const rotationChampionIds: RotationIds = rotationData.freeChampionIds;
-
-      // 전체 챔피언 정보 불러오기
-      const allChampions = await getChampionData();
-      const championsArray: Champion[] = Object.values(allChampions);
-
-      const championsDetails: (Champion | undefined)[] = await Promise.all(
-        rotationChampionIds.map(async (id) => {
-          const championDetail = championsArray.find(
-            (all) => all.key === id.toString()
-          );
-          console.log(championDetail);
-
-          if (!championDetail) {
-            console.log(`ID ${id}에 해당하는 챔피언을 찾을 수 없습니다.`);
-          }
-          return championDetail;
-        })
-      );
-      // undefined배열일 때 제외하는 필터링
-      const filteredChampions = championsDetails.filter(
-        // 타입 가드
-        (champion): champion is Champion => champion !== undefined
-      );
-
-      setRotationChampions(filteredChampions);
-      console.log('로테이션 챔피언 세부 정보:', filteredChampions);
     };
 
     fetchData();
   }, []);
 
+  if (loading) {
+    return <p>로테이션 챔피언을 불러오는 중입니다!!!</p>;
+  }
+
   console.log(rotationChampions);
   return (
     <div className='mb-12 mt-10'>
       <h1 className='text-center text-lg font-bold mb-6'>챔피언 로테이션</h1>
-      {rotationChampions.length === 0 ? (
-        <p>로테이션 챔피언을 불러오는 중입니다...</p>
-      ) : (
-        <ul className='grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 w-full mt-3 p-5'>
-          {rotationChampions.map((champion) => (
-            <li key={champion.id}>
-              <Link href={`/champions/${champion.id}`}>
-                <Image
-                  width={150}
-                  height={150}
-                  alt={champion.name}
-                  src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/champion/${champion.image.full}`}
-                />
-                <p className='font-bold text-lg'>{champion.name}</p>
-                <p>{champion.title}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
+
+      <ul className='grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 w-full mt-3 p-5'>
+        {rotationChampions.map((champion) => (
+          <li key={champion.id}>
+            <Link href={`/champions/${champion.id}`}>
+              <Image
+                width={150}
+                height={150}
+                alt={champion.name}
+                src={`https://ddragon.leagueoflegends.com/cdn/14.19.1/img/champion/${champion.image.full}`}
+              />
+              <p className='font-bold text-lg'>{champion.name}</p>
+              <p>{champion.title}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
