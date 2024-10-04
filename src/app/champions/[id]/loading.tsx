5b8a4c67766f73b7705b97cb9bde1c869e73a418
Bsjaehee <bsjaehee94@gmail.com>
2024-10-04 15:29:38 +0900

    add: 챔피언 상세 로딩페이지 추가

---
+++ src/app/champions/[id]/loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className='grid place-items-center min-h-screen text-center mt-10'>
+      <h2 className='text-xl font-bold mb-4'>챔피언 상세 페이지 로딩중...</h2>
+      <p>잠시만 기다려 주세요.</p>
+    </div>
+  );
+};
+
+export default Loading;
