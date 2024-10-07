5f2a26a17b62e308d1930a4f82b7d0bd49fc48bd
Bsjaehee <bsjaehee94@gmail.com>
2024-10-07 12:12:39 +0900

    fix: 다크모드버튼 useId 추가

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -2,17 +2,19 @@
 import { useThemeStore } from '@/app/store/useThemeStore';
 import { Switch } from './ui/switch';
 import { Label } from './ui/label';
+import { useId } from 'react';
 
 const DarkModeButton = () => {
   const { darkMode, toggleDarkMode } = useThemeStore();
+  const toggleSwitch = useId();
 
   return (
     <div className='flex flex-row items-center justify-center mt-3 mr-2'>
       <div className='ml-auto'>
-        <Label className='text-4xl' htmlFor='toggleSwitch'>
+        <Label className='text-4xl' htmlFor={toggleSwitch}>
           {darkMode ? '☀️' : '🌕'}
         </Label>
-        <Switch id='toggleSwitch' onClick={toggleDarkMode} className='' />
+        <Switch id={toggleSwitch} onClick={toggleDarkMode} className='' />
       </div>
     </div>
   );
