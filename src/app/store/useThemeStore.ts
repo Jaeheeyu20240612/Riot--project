da8802e684bd97d23d6058df6a8a14079508a776
Bsjaehee <bsjaehee94@gmail.com>
2024-10-08 14:41:21 +0900

    fix: 주스탠드 스토어 수정

--- src/app/store/useThemeStore.ts
+++ src/app/store/useThemeStore.ts
@@ -1,24 +1,34 @@
 import { create } from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 
 interface useThemeStoreProps {
   darkMode: boolean;
   toggleDarkMode: () => void;
 }
 
-export const useThemeStore = create(
-  persist<useThemeStoreProps>(
+// export const useThemeStore = create(
+//   persist<useThemeStoreProps>(
+//     (set) => ({
+//       darkMode: false,
+//       toggleDarkMode: () => {
+//         set((state) => ({
+//           darkMode: !state.darkMode,
+//         }));
+//       },
+//     }),
+//     {
+//       name: 'theme-storage', // localStorage에 'theme-storage'라는 키로 상태를 저장
+//       storage: createJSONStorage(() => localStorage),
+//     }
+//   )
+// );
+
+export const useThemeStore = create<useThemeStoreProps>()(
+  persist(
     (set) => ({
       darkMode: false,
-      toggleDarkMode: () => {
-        set((state) => ({
-          darkMode: !state.darkMode,
-        }));
-      },
+      toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
     }),
-    {
-      name: 'theme-storage', // localStorage에 'theme-storage'라는 키로 상태를 저장
-      storage: createJSONStorage(() => localStorage),
-    }
+    { name: 'theme-stroe' }
   )
 );
